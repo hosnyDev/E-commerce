c76b58d8373596334cea4a4135593f2cb4d4412e
hosnyDev <hosny.dev@gmail.com>
2020-12-01 02:16:38 +0200

    SplashActivity

---
+++ app/src/main/java/com/hosnydevtest/shopapp/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.hosnydevtest.shopapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hosnydevtest.shopapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/hosnydevtest/shopapp/ui/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.hosnydevtest.shopapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.hosnydevtest.shopapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+                finish();
+            }
+        },2000);
+
+
+    }
+}
